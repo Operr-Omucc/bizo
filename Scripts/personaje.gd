ac8ed816c7d670d3497a7a42c3c1d09e4bd97e7b
Operr-Omucc <francomarchiori2006@gmail.com>
2025-07-10 14:48:47 -0300

    small fix
    fix a la habilidad de dashear

--- Scripts/personaje.gd
+++ Scripts/personaje.gd
@@ -10,9 +10,11 @@ func _ready():
 
 #Movimiento basico
 func _physics_process(_delta):
+	
 	var direccion = Input.get_vector("Izquierda","Derecha","Arriba","Abajo")
 	velocity = direccion * speed
-	
+	if Input.is_action_just_pressed("Dash") && cooldown:
+		start_dash()
 	#is_mouse_left()
 	move_and_slide()
 	
@@ -20,20 +22,8 @@ func _physics_process(_delta):
 #Funcion de dasheo
 func start_dash():
 		cooldown = false
-		speed = 1500
+		speed = speed + 1500
 		await get_tree().create_timer(0.3).timeout
-		speed = 500
+		speed = speed - 1500
 		await get_tree().create_timer(1.00).timeout
 		cooldown = true
-
-#Funcion que detecta si el personaje mira para la izquierda o no
-func is_mouse_left():
-
-	#var alsod = 0 < get_position_delta().x 
-	var onLeftSide = 0<get_global_mouse_position().x
-	
-	if onLeftSide:
-		self.scale.x = -1
-	else:
-		self.scale.x = 1
-	
--- Scripts/personaje_enojado.gd
+++ Scripts/personaje_enojado.gd
@@ -3,4 +3,3 @@ extends 'res://Scripts/personaje.gd'
 func _ready():
 	speed = 350
 	self.add_to_group("personaje")
-	
