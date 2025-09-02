bb06fe7dd4f3ca6c47d71e2862fa9d2584e1ce80
Operr <francomarchiori2006@gmail.com>
2025-09-02 20:37:13 -0300

    cambios al balance de thor
    cambio pequeño (+cooldown)

--- Scripts/martillothor.gd
+++ Scripts/martillothor.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 var puede_golpear: bool
 var cooldown = true
+var CeroCooldown: bool = true
 
 var fire_rate: float
 var posicion_original: Vector2
@@ -39,12 +40,12 @@ func fire():
 	AudioManager.play_disparo()
 	if not cooldown:
 		return
-	var ray = preload("res://Escenas/Rayo.tscn").instantiate()
+	var ray = preload("res://Escenas/rayo.tscn").instantiate()
 	
 	#establece la posicion, rotacion y direccion de la bala
 	ray.pos = muzzle.global_position
 	ray.rota = muzzle.global_rotation
-	ray.dir = Vector2.RIGHT.rotated(muzzle.global_rotation) # opcional
+	ray.dir = Vector2.RIGHT.rotated(muzzle.sglobal_rotation) # opcional
 	
 	#añade la bala al arbol actual
 	get_tree().current_scene.add_child(ray)
@@ -56,22 +57,28 @@ func fire():
 func CeroMetralleta():
 	var rando = randi_range(10, 100)
 	var muzzle: Marker2D = $"Hombro/Sprite2D/Puño/Marker2D"
-	for i in rando:
-		var ray = preload("res://Escenas/Rayo.tscn").instantiate()
-		
-		# Copiamos la posición del muzzle y le agregamos un Y aleatorio
-		var pos = muzzle.global_position
-		
-		# Dirección hacia el mouse
-		ray.pos = muzzle.global_position
-		ray.pos.y += randi_range(-600, 600)
-		ray.rota = muzzle.global_rotation
-		ray.dir = Vector2.RIGHT.rotated(muzzle.global_rotation) # opcional
-		
-		# Añade la bala al árbol
-		get_tree().current_scene.add_child(ray)
+	
+	if CeroCooldown == true:
+		CeroCooldown = false
+		for i in rando:
+			
+			var ray = preload("res://Escenas/rayo.tscn").instantiate()
+			
+			# Copiamos la posición del muzzle y le agregamos un Y aleatorio
+			var pos = muzzle.global_position
+			
+			# Dirección hacia el mouse
+			ray.pos = muzzle.global_position
+			ray.pos.y += randi_range(-600, 600)
+			ray.rota = muzzle.global_rotation
+			ray.dir = Vector2.RIGHT.rotated(muzzle.global_rotation) # opcional
+			
+			# Añade la bala al árbol
+			get_tree().current_scene.add_child(ray)
 
-		# Añade Cooldown
-		cooldown = false
-		await get_tree().create_timer(0.1).timeout
-		cooldown = true
+			# Añade Cooldown
+			cooldown = false
+			await get_tree().create_timer(0.1).timeout
+			cooldown = true
+		await get_tree().create_timer(10).timeout
+		CeroCooldown = true
